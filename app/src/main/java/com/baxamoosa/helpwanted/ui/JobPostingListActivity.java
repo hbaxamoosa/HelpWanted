41aeac0ea2f19395d08b4ea1889346ec1a1d2a0a
Hasnain Baxamoosa <hbaxamoosa@gmail.com>
2016-04-20 08:27:02 -0500

    Updated JobPostingListActivity.java and associated layouts to replace ListView with CardView.

--- app/src/main/java/com/baxamoosa/helpwanted/ui/JobPostingListActivity.java
+++ app/src/main/java/com/baxamoosa/helpwanted/ui/JobPostingListActivity.java
@@ -210,13 +210,14 @@ public class JobPostingListActivity extends AppCompatActivity {
         @Override
         public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             View view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.jobposting_list_content, parent, false);
+                    .inflate(R.layout.cardview_jobpost, parent, false);
             return new ViewHolder(view);
         }
 
         @Override
         public void onBindViewHolder(final ViewHolder holder, int position) {
             holder.mItem = mValues.get(position);
+            holder.mOverlayTextView.setText(mValues.get(position).id);
             holder.mIdView.setText(mValues.get(position).id);
             holder.mContentView.setText(mValues.get(position).content);
 
@@ -249,6 +250,7 @@ public class JobPostingListActivity extends AppCompatActivity {
 
         public class ViewHolder extends RecyclerView.ViewHolder {
             public final View mView;
+            public final TextView mOverlayTextView;
             public final TextView mIdView;
             public final TextView mContentView;
             public DummyContent.DummyItem mItem;
@@ -256,6 +258,7 @@ public class JobPostingListActivity extends AppCompatActivity {
             public ViewHolder(View view) {
                 super(view);
                 mView = view;
+                mOverlayTextView = (TextView) view.findViewById(R.id.overlaytext);
                 mIdView = (TextView) view.findViewById(R.id.id);
                 mContentView = (TextView) view.findViewById(R.id.content);
             }
